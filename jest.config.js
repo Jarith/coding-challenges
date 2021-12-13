e12f1374a948e596384c4b290cac304ef471952c
jarith <gorodilov.box@gmail.com>
2021-12-13 06:20:03 +0300

    (feat): add eslint + jest configs

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const config = {
+    testMatch: [
+        '**/spec/**/*.spec.js',
+    ],
+    roots: [
+        '<rootDir>/src/',
+    ],
+    modulePaths: [
+        '<rootDir>',
+        '<rootDir>/src',
+    ],
+    transform: {},
+};
+
+export default config;
